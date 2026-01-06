a2db93c1589b1b0e4bd3615fdbe6e25ee1b2beb8
memit0 <mebattll@gmail.com>
2026-01-06 13:49:09 -0500

    Added the default four buttons

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 const container = document.querySelector(".container");
+const gridButton = document.querySelector(".grid-btn");
+const clearButton = document.querySelector(".clear-btn");
+const colorButton = document.querySelector(".color-btn");
+const opacityButton = document.querySelector(".opacity-btn");
 const fragment = document.createDocumentFragment();
-const grid = 16 * 16;
 
-for (let i = 0; i < grid; i++) {
-  const div = document.createElement("div");
-  fragment.appendChild(div);
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "black";
-  });
+function createGrid(length) {
+  const grid = length * length;
+
+  const squareLength = 960 / length;
+  for (let i = 0; i < grid; i++) {
+    const div = document.createElement("div");
+    fragment.appendChild(div);
+    div.style.height = squareLength + "px";
+    div.style.width = squareLength + "px";
+    div.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = "black";
+    });
+  }
+  container.appendChild(fragment);
 }
 
-container.appendChild(fragment);
+function clearGrid() {
+  const div = container.querySelectorAll("div");
+  div.forEach((div) => div.remove());
+}
+
+createGrid(16);
+
+gridButton.addEventListener("click", () => {
+  const maxGridSize = 100;
+
+  let userLength = Number(
+    window.prompt(`What should the grid size be? (1-${maxGridSize})`, ""),
+  );
+  if (1 <= userLength && userLength < maxGridSize) {
+    // User input a valid number
+    clearGrid();
+    createGrid(userLength);
+  } else {
+    // User entered invalid number
+    alert("Please enter a valid number");
+  }
+});
+
+colorButton.addEventListener("click", () => {});
+
+opacityButton.addEventListener("click", () => {});
